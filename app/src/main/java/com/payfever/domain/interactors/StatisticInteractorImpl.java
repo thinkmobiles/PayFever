60ad58b931ac06437b27802f41bcf7927c634af0
rMozes <richard.mozes@thinkmobiles.com>
2015-10-19 11:14:14 +0200

    *  Domain Layer:   1. Interactors

--- app/src/main/java/com/payfever/domain/interactors/StatisticInteractorImpl.java
+++ app/src/main/java/com/payfever/domain/interactors/StatisticInteractorImpl.java
@@ -1,9 +1,23 @@
 package com.payfever.domain.interactors;
 
+
+import android.os.Handler;
+
+import com.payfever.domain.BaseInteractor;
+
+import rx.Observable;
+
 /**
  * Created by richi on 2015.10.19..
  */
-public class StatisticInteractorImpl extends StatisticInteractor {
+public class StatisticInteractorImpl extends BaseInteractor {
 
+    public StatisticInteractorImpl(Handler _handler) {
+        super(_handler);
+    }
 
+    @Override
+    public Observable buildGetObserver() {
+        return null;
+    }
 }
