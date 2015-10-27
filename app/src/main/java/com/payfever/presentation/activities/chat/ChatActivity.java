69cb32a68cdf7c491c49117954370189143be3ca
mRogach <Misukrogach@ukr.net>
2015-10-27 10:14:52 +0300

    chat in progress

---
+++ app/src/main/java/com/payfever/presentation/activities/chat/ChatActivity.java
@@ -0,0 +1,34 @@
+package com.payfever.presentation.activities.chat;
+
+import android.os.Bundle;
+
+import com.payfever.presentation.basics.BaseActivity;
+
+/**
+ * Created by
+ * mRogach on 27.10.2015.
+ */
+
+public final class ChatActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public int getToolbarId() {
+        return 0;
+    }
+
+    @Override
+    public int getContainerId() {
+        return 0;
+    }
+
+    @Override
+    public int getProgressId() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/payfever/presentation/activities/chat/ChatAdapter.java
@@ -0,0 +1,33 @@
+package com.payfever.presentation.activities.chat;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by
+ * mRogach on 27.10.2015.
+ */
+
+public final class ChatAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
