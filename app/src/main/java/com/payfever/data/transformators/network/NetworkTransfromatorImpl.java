8a41f328277b5aa419dc44aae2cae2f9646a7454
rMozes <richard.mozes@thinkmobiles.com>
2015-11-04 12:49:23 +0100

    * Network Account FirstUserLevel is done

--- app/src/main/java/com/payfever/data/transformators/network/NetworkTransfromatorImpl.java
+++ app/src/main/java/com/payfever/data/transformators/network/NetworkTransfromatorImpl.java
@@ -1,10 +1,14 @@
 package com.payfever.data.transformators.network;
 
 import com.parse.ParseObject;
+import com.payfever.data.model.network.NetworkUser;
 import com.payfever.data.model.network.NetworkUserResponse;
+import com.payfever.data.model.network.parse.NetworkParseWrapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by richi on 2015.11.02..
@@ -12,10 +16,29 @@ import java.util.Map;
 public class NetworkTransfromatorImpl implements NetworkTransformator {
     @Override
     public NetworkUserResponse transform(Map<String, Object> _data) {
+        List<NetworkUser> networkUsers = new ArrayList<>();
         NetworkUserResponse response = new NetworkUserResponse();
-//        List<ParseObject> firstLevel = _data.get("firstLevel");
-//        List<ParseObject> networkLevel = _data.get("networkLevel");
+        List<Map<String, Object>> firstLevel = (List<Map<String, Object>>) _data.get("firstLevel");
+        for (Map<String, Object> userMap : firstLevel) {
+            NetworkUser networkUser = transform((NetworkParseWrapper) userMap.get("item"));
+            networkUser.setmIsFirstLevel(true);
+            if (networkUser != null)
+                networkUsers.add(networkUser);
+        }
+
+        response.setmUsers(networkUsers);
 
         return response;
+    }
+
+    @Override
+    public NetworkUser transform(NetworkParseWrapper _parseObject) {
+        if (!_parseObject.isAccepted())
+            return null;
+        NetworkUser networkUser = new NetworkUser();
+        networkUser.setUserName(_parseObject.getUserName());
+        networkUser.setmPushChannel(_parseObject.getPushChannel());
+        networkUser.setmPhoneNumber(_parseObject.getPhoneNumber());
+        return networkUser;
     }
 }
